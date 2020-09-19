a60a96b76fd81c91b36a1dbe51215f406ae9a5a6
n-a-m-e <n-a-m-e@users.noreply.github.com>
2020-09-19 16:37:53 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,16 @@ set -e
 
 # initialize on first run
 echo "Initializing..."
-mkdir -p /var/log/dovecot /var/log/getmail
+mkdir -p /var/log/dovecot /var/log/offlineimap
+
 touch /var/log/dovecot/dovecot.log
 chown root:users /var/log/dovecot/dovecot.log
 chmod 664 /var/log/dovecot/dovecot.log
+
+touch /var/log/offlineimap/offlineimap.log
+chown root:users /var/log/offlineimap/offlineimap.log
+chmod 664 /var/log/offlineimap/offlineimap.log
+
 for USER in $(ls -1 /home); do
   echo "User '$USER':"
   if ! id -u "$USER" >/dev/null 2>&1; then
@@ -17,16 +23,8 @@ for USER in $(ls -1 /home); do
     useradd --groups=users --no-create-home --shell='/bin/true' "$USER"
     echo -e "$DEFAULT_PASSWD\n$DEFAULT_PASSWD\n" | passwd "$USER"
     chown -R "$USER:$USER" "/home/$USER"
-    chmod 700 /home/$USER/{Maildir,sieve,.getmail} || true
+    chmod 700 /home/$USER/{Maildir,sieve,offlineimap} || true
   fi
-  for RC in $(ls -1 /home/$USER/.getmail/getmailrc-*); do
-    echo "- $RC"
-    # fix log permissions
-    LOG="/var/log/getmail/${RC##*/getmailrc-}.log"
-    touch "$LOG"
-    chown "$USER:$USER" "$LOG"
-    chmod 644 "$LOG"
-  done
 done
 
 # start services
